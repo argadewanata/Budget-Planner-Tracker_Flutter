b9b087be26297a3f6a77cedbabfd02fea1586f25
argadewanata <70679432+argadewanata@users.noreply.github.com>
2024-05-27 21:16:22 +0700

    feat: navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:budgetplannertracker/pages/profile_page.dart';
+import 'package:budgetplannertracker/pages/save_page.dart';
+import 'package:budgetplannertracker/pages/travel_page.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    TravelPage(),
+    SavePage(),
+    ProfilePage(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Travel Budget Tracker"),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.card_travel),
+            label: "Travel Plan",
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.attach_money),
+            label: "Save",
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.account_circle),
+            label: "Profile",
+          ),
+        ]
+      ),
+    );
+  }
+
+  void onTabTapped(int index){
+    setState(() {
+      _currentIndex = index;
+    });
+
   }
 }
+
